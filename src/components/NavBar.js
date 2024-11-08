b55b5235d680cfc7bbfd11a4971358b2252355ab
Irish Manalang <irish.r.manalang.mil@army.mil>
2024-11-08 08:40:08 -0600

    completed initial setup and working on Logo

---
+++ src/components/NavBar.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Link from "next/link";
+import Logo from "@/components/Logo";
+
+const NavBar = () => {
+    return (
+        <header
+            className='w-full px-32 py-8 font-medium flex items-center justify-between'
+        >
+            <nav>
+                <Link href='/'>Home</Link>
+                <Link href='/about'>About</Link>
+                <Link href='/projects'>Projects</Link>
+                <Link href='/articles'>Articles</Link>
+            </nav>
+
+            <Logo/>
+
+            <nav>
+                <Link href='/' target={"_blank"}>T</Link>
+                <Link href='/' target={"_blank"}>T</Link>
+                <Link href='/' target={"_blank"}>T</Link>
+                <Link href='/' target={"_blank"}>T</Link>
+                <Link href='/' target={"_blank"}>T</Link>
+            </nav>
+
+        </header>
+    );
+};
+
+export default NavBar;
