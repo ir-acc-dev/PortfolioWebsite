b24fe997245bbd8b6e0e2506819ee98daf3053a0
Irish Manalang <irish.r.manalang.mil@army.mil>
2024-11-08 16:17:51 -0600

    working on navbar styling, updating next.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,24 @@
 import React from 'react';
 import Link from "next/link";
 import Logo from "@/components/Logo";
+import {useRouter} from "next/router";
+
+const CustomLink = ({href, title, className=""}) => {
+    const router = useRouter();
+    return (
+        <Link href={href} className={`${className} relative group`}>
+            {title}
+
+            <span className={`h-[1px] inline-block w-0 bg-dark
+            absolute left-0 -bottom-0.5
+            group-hover:w-full
+            transition-[width] ease duration-300
+            ${router.asPath === href ? 'w-full' : 'w-0'}
+            `}>&nbsp;</span>
+
+        </Link>
+    )
+}
 
 const NavBar = () => {
     return (
@@ -8,13 +26,13 @@ const NavBar = () => {
             className='w-full px-32 py-8 font-medium flex items-center justify-between'
         >
             <nav>
-                <Link href='/'>Home</Link>
-                <Link href='/about'>About</Link>
-                <Link href='/projects'>Projects</Link>
-                <Link href='/articles'>Articles</Link>
+                <CustomLink href='/' title="Home" className='mr-4'></CustomLink>
+                <CustomLink href='about' title="About" className='mx-4'></CustomLink>
+                <CustomLink href='projects' title="Projects" className='mx-4'></CustomLink>
+                <CustomLink href='articles' title="Articles" className='ml-4'></CustomLink>
             </nav>
 
-            <Logo/>
+
 
             <nav>
                 <Link href='/' target={"_blank"}>T</Link>
@@ -23,6 +41,10 @@ const NavBar = () => {
                 <Link href='/' target={"_blank"}>T</Link>
                 <Link href='/' target={"_blank"}>T</Link>
             </nav>
+
+            <div className='absolute left-[50%] top-2 translate-x-[-50%]'>
+                <Logo/>
+            </div>
 
         </header>
     );
