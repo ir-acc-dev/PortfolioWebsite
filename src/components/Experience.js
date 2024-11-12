f672dccffa0095a1b4bde7183710cef7770a23be
Irish Manalang <irish.r.manalang.mil@army.mil>
2024-11-12 13:59:23 -0600

    updated experience section

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -17,13 +17,13 @@ const Experience = () => {
             transition={{duration: 0.5, type: "spring"}}
             >
                 <h3 className='capitalize font-bold text-2xl sm:text-xl xs:text-lg'>
-                    {position}&nbsp;<a href={companyLink} target={'_blank'} className='text-primary capitalize'>@{company}</a></h3>
+                    {position}&nbsp;<a href={companyLink} target={'_blank'} className='text-blue-500 capitalize'>@{company}</a></h3>
 
                 <span className='capitalize font-medium text-dark/75 xs:text-sm'>
                     {time} | {address}
                 </span>
 
-                <p className='font-medium w-full md:text-sm'>{address}
+                <p className='font-medium w-full md:text-sm'>
                     {work}
                 </p>
 
@@ -55,39 +55,33 @@ const Experience = () => {
 
                 <ul className='w-full flex flex-col items-start justify-between ml-4 xs:ml-2'>
                     <Details
-                        position="Software Engineer" company="US Army"
+                        position="Software Engineer" company=" US Army - Futures Command"
                         companyLink="https://www.army.mil"
                         time="2024-Present" address="Austin, TX"
-                        work='I started working in the Army Software Factory after being selected to particiate in Cohort 8. As a software developer I am still in training but excited to learn more things to innovate. '
+                        work='Currently in training to develop full-stack applications to solve complex Army challenges'
                     />
 
                     <Details
-                        position="Software Engineer" company="US Army"
+                        position="Executive Officer" company=" US Army - Europe and Africa"
                         companyLink="https://www.army.mil"
-                        time="2024-Present" address="Austin, TX"
-                        work='I started working in the Army Software Factory after being selected to particiate in Cohort 8. As a software developer I am still in training but excited to learn more things to innovate. '
+                        time="2021-2024" address="Kaiserslautern, Germany"
+                        work='Oversaw the coordination and execution of critical events for a senior executive in the US Army'
                     />
 
                     <Details
-                        position="Software Engineer" company="US Army"
+                        position="Platoon Leader" company=" US Army - 65th Medical BDE"
                         companyLink="https://www.army.mil"
-                        time="2024-Present" address="Austin, TX"
-                        work='I started working in the Army Software Factory after being selected to particiate in Cohort 8. As a software developer I am still in training but excited to learn more things to innovate. '
+                        time="2020-2021" address="Camp Casey, South Korea"
+                        work='Led a medical platoon, ensuring operational readiness, team development, and mission success in dynamic environments'
                     />
 
                     <Details
-                        position="Software Engineer" company="US Army"
-                        companyLink="https://www.army.mil"
-                        time="2024-Present" address="Austin, TX"
-                        work='I started working in the Army Software Factory after being selected to particiate in Cohort 8. As a software developer I am still in training but excited to learn more things to innovate. '
+                        position="IT Specialist" company=" Santa Clara University"
+                        companyLink="https://www.scu.edu/"
+                        time="2017-2020" address="Santa Clara, CA"
+                        work='Provided reliable IT assistance and account management for over 8,000 university personnel'
                     />
 
-                    <Details
-                        position="Software Engineer" company="US Army"
-                        companyLink="https://www.army.mil"
-                        time="2024-Present" address="Austin, TX"
-                        work='I started working in the Army Software Factory after being selected to particiate in Cohort 8. As a software developer I am still in training but excited to learn more things to innovate. '
-                    />
 
                 </ul>
             </div>
--- src/pages/articles.js
+++ src/pages/articles.js
@@ -3,7 +3,8 @@ import Layout from "@/components/Layout";
 import AnimatedText from "@/components/AnimatedText";
 import Image from "next/image";
 import Link from "next/link";
-import ArticleImage from "../../public/images/articles/create loading screen in react js.jpg"
+import ProjectArticleImg from "../../public/images/articles/ProjectArticleImg.jpg"
+import LearningResourcesImg from "../../public/images/articles/LearningResourcesImg.jpg"
 import {motion} from "framer-motion";
 
 const FramerImage = motion(Image);
@@ -34,15 +35,12 @@ const FeaturedArticle = ({img, title, time, summary, link}) => {
     )
 }
 
-const Article = ({title, date, link}) => {
+const Article = ({title, link}) => {
     return (
         <li className='relative w-full p-4 py-6 my-4 rounded-xl flex items-center justify-between bg-light text-dark first: mt-0 border border-solid border-dark border-r-4 border-b-4 sm:flex-col'>
-
             <Link href={link} target='_blank'>
                 <h2 className='capitalize text-xl font-semibold hover:underline'>{title}</h2>
             </Link>
-
-            <span className='text-primary font-semibold pl-4 sm:self-start xs:text-sm'>{date}</span>
         </li>
     )
 }
@@ -57,58 +55,48 @@ const Articles = () => {
 
             <main className='w-full mb-16 flex flex-col items-center justify-center overflow-hidden'>
                 <Layout className="pt-16">
-                    <AnimatedText text='Some of My Articles'
+                    <AnimatedText text='Tips & Guides for New Devs'
                                   className='mb-16 lg:!text-7xl sm:mb-8 sm:!text-6xl xs:!text-4xl'/>
 
+                    {/*FEATURED ARTICLES SECTION*/}
                     <ul className='grid grid-cols-2 gap-16 lg:gap-8 md:grid-cols-1 md:gap-y-16'>
-                        <li>
-                            <FeaturedArticle
-                                title='Article Title'
-                                summary='Article Summary'
-                                time='4 mins'
-                                link='/'
-                                img={ArticleImage}/>
-                        </li>
 
-
-                        <li>
                             <FeaturedArticle
-                                title='Article Title'
-                                summary='Article Summary'
-                                time='4 mins'
-                                link='/'
-                                img={ArticleImage}/>
-                        </li>
+                                title='Full Stack Project Walkthrough'
+                                summary='Details the process of creating a full stack application with all CRUD functions'
+                                time='10 mins'
+                                link='https://docs.google.com/document/d/1K5Xo92TRXlu79eYlQQReQrZNwljfjueEdcI_YsMamoM/edit?tab=t.0#heading=h.m55knbq82hpp'
+                                img={ProjectArticleImg}/>
+
+                            <FeaturedArticle
+                                title='Learning Resources'
+                                summary='Resources that helped me the most in the dev accelerator'
+                                time='2 mins'
+                                link='https://docs.google.com/document/d/1yYqLzMZYhsODuCwJtrFwW-tNCuTAiUqDnfjVIRqO3aY/edit?tab=t.0#heading=h.sdfdba779ist'
+                                img={LearningResourcesImg}/>
+
                     </ul>
 
                     <h2 className='font-bold text-4xl w-full text-center my-16 mt-32'>
-                        All Articles
+                        All Other Guides
                     </h2>
 
                     <ul>
                         <Article
-                            title='Article Title - a smaller one'
-                            date="Feb 14, 2023"
+                            title='Fullstack - Template w/ Testing'
                             link='/'
                         />
 
                         <Article
-                            title='Article Title - a smaller one'
-                            date="Feb 14, 2023"
+                            title='Tailwind CSS - Frontend Template'
                             link='/'
                         />
 
                         <Article
-                            title='Article Title - a smaller one'
-                            date="Feb 14, 2023"
+                            title="Where's that doc?"
                             link='/'
                         />
 
-                        <Article
-                            title='Article Title - a smaller one'
-                            date="Feb 14, 2023"
-                            link='/'
-                        />
                     </ul>
 
                 </Layout>
